4593b4629b1225d34a31be6af6d54745e23da517
zoryana-kvitochka <stanislava.moskalets@gmail.com>
2022-09-23 18:30:15 +0300

    fixed api integration, unit conversion, added time last updated, wind speed, weather description, high low temp, weather icons

--- app.js
+++ app.js
@@ -1,46 +1,16 @@
-function formatDate(date) {
-  let currentWeatherDate = document.querySelector("#current-weather-date");
-  let now = new Date();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[now.getDay()];
-  let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-
-  let month = months[now.getMonth()];
-
-  function hoursCalc(now) {
-    return (now.getHours() < 10 ? "0" : "") + now.getHours();
-  }
-  function minutesCalc(now) {
-    return (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
-  }
-
-  let hours = hoursCalc(now);
-  let minutes = minutesCalc(now);
-
-  currentWeatherDate.innerHTML = `${day}, ${month} ${now.getDate()}, ${hours}:${minutes}`;
-}
-formatDate();
+let imperialcalc = document.querySelector("#imperial-btn");
+let temperatureElement = document.querySelector("#current-temperature");
+let windSpeedElement = document.querySelector("#wind-speed");
+let originalWindSpeed = null;
+let humidityElement = document.querySelector("#current-humidity");
+let currentWeatherDescription = document.querySelector(
+  "#current-weather-description"
+);
+let currentTemperatureMin = document.querySelector("#current-temp-min");
+let currentTemperatureMax = document.querySelector("#current-temp-max");
+let currentWeatherDate = document.querySelector("#current-weather-date");
+let iconElement = document.querySelector("#current-weather-icon");
+getGeolocationForecast();
 
 let form = document.querySelector("#form");
 let searchInput = document.querySelector("#input-text");
@@ -63,25 +33,40 @@ function capitalize(searchInput) {
 
 form.addEventListener("submit", currrentLocation);
 
-let fahrenheitcalc = document.querySelector("#fahrenheit-link");
-let temperatureElement = document.querySelector("#temperature");
-let temperature = temperatureElement.innerHTML;
-function cToF(degrees) {
-  degrees.preventDefault();
-  degrees = parseInt(temperature) * 1.8 + 32;
-  temperatureElement.innerHTML = parseInt(degrees);
+function mToI() {
+  if (temperatureElement.innerHTML.split("").includes("F")) {
+    return;
+  }
+  let degreesMain = parseFloat(temperatureElement.innerHTML) * 1.8 + 32;
+  let degreesMin = parseFloat(currentTemperatureMin.innerHTML) * 1.8 + 32;
+  let degreesMax = parseFloat(currentTemperatureMax.innerHTML) * 1.8 + 32;
+  temperatureElement.innerHTML = `${Math.round(degreesMain)} \u00B0 F`;
+  currentTemperatureMin.innerHTML = `${Math.round(degreesMin)} \u00B0 F`;
+  currentTemperatureMax.innerHTML = `${Math.round(degreesMax)} \u00B0 F`;
+  let speed = parseFloat(windSpeedElement.innerHTML) * 0.62137;
+  windSpeedElement.innerHTML = `${Math.round(speed)} Mph`;
 }
 
-fahrenheitcalc.addEventListener("click", cToF);
-let celciuscalc = document.querySelector("#celcius-link");
+imperialcalc.addEventListener("click", mToI);
+let metriccalc = document.querySelector("#metric-btn");
 
-function fToC(degrees1) {
-  degrees1.preventDefault();
-  degrees1 = parseInt(temperature);
-  temperatureElement.innerHTML = parseInt(degrees1);
+function IToM() {
+  if (temperatureElement.innerHTML.split("").includes("C")) {
+    return;
+  }
+  let temp = parseFloat(temperatureElement.innerHTML) - 32;
+  let tempMin = parseFloat(currentTemperatureMin.innerHTML) - 32;
+  let tempMax = parseFloat(currentTemperatureMax.innerHTML) - 32;
+  let degreesMain = temp / 1.8;
+  let degreesMin = tempMin / 1.8;
+  let degreesMax = tempMax / 1.8;
+  temperatureElement.innerHTML = `${Math.round(degreesMain)} \u00B0 C`;
+  currentTemperatureMin.innerHTML = `${Math.round(degreesMin)} \u00B0 C`;
+  currentTemperatureMax.innerHTML = `${Math.round(degreesMax)} \u00B0 C`;
+  windSpeedElement.innerHTML = `${originalWindSpeed} km/h`;
 }
 
-celciuscalc.addEventListener("click", fToC);
+metriccalc.addEventListener("click", IToM);
 
 function handlePosition(position) {
   let lat = parseFloat(position.coords.latitude);
@@ -91,11 +76,29 @@ function handlePosition(position) {
   axios.get(apiURL).then(showTemperature);
   let apiURLCity = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`;
   axios.get(apiURLCity).then(showCurrentCity);
+  axios.get(apiURL).then(formatDate);
 }
 
 function showTemperature(response) {
-  let forecast = document.querySelector("#temperature");
-  forecast.innerHTML = `${Math.round(response.data.main.temp)} `;
+  temperatureElement.innerHTML = `${Math.round(
+    response.data.main.temp
+  )} \u00B0 C`;
+  windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+  originalWindSpeed = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = `${response.data.main.humidity} %`;
+  currentWeatherDescription.innerHTML = response.data.weather[0].description;
+  currentTemperatureMin.innerHTML = `${Math.round(
+    response.data.main.temp_min
+  )} \u00B0 C`;
+  currentTemperatureMax.innerHTML = `${Math.round(
+    response.data.main.temp_max
+  )} \u00B0 C`;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", `${response.data.weather[0].description}`);
+  currentWeatherDate.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function showCurrentCity(cityResponse) {
@@ -108,3 +111,41 @@ function getGeolocationForecast() {
 let geolocationBtn = document.querySelector("#currentGeolocation");
 
 geolocationBtn.addEventListener("click", getGeolocationForecast);
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+  return `${day}, ${month} ${date.getDate()} ${hours}:${minutes}`;
+}
